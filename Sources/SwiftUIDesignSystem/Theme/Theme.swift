371a485d944806f1373d4b3cd32328aef0dcec15
Jason Lew-Rapai <jasonjrr@users.noreply.github.com>
2024-04-22 15:11:46 -0700

    Remove unused localizations

--- Sources/SwiftUIDesignSystem/Theme/Theme.swift
+++ Sources/SwiftUIDesignSystem/Theme/Theme.swift
@@ -37,38 +37,18 @@ extension Theme {
         public let animationShortDistance: Double
         public let animationProgressSpinnerRevolution: Double
         
-        public let listItemCardCornerRadius: Double
-        public let listItemSquareImageCornerRadius: Double
-        
-        public let questionFieldCardCornerRadius: Double
-        public let questionFieldMinHeight: Double
-        public let questionFieldShadowRadius: Double
-        public let questionFieldShadowYOffset: Double
-        
         public init(
             disabledOpacity: Double = 0.3,
             minimumButtonSize: Double = 44.0,
             recommendedButtonSize: Double = 48.0,
             animationShortDistance: Double = 0.18,
-            animationProgressSpinnerRevolution: Double = 1.75,
-            listItemCardCornerRadius: Double = 8.0,
-            listItemSquareImageCornerRadius: Double = 4.0,
-            questionFieldCardCornerRadius: Double = 16.0,
-            questionFieldMinHeight: Double = 56.0,
-            questionFieldShadowRadius: Double = 32.0,
-            questionFieldShadowYOffset: Double = 4.0
+            animationProgressSpinnerRevolution: Double = 1.75
         ) {
             self.disabledOpacity = disabledOpacity
             self.minimumButtonSize = minimumButtonSize
             self.recommendedButtonSize = recommendedButtonSize
             self.animationShortDistance = animationShortDistance
             self.animationProgressSpinnerRevolution = animationProgressSpinnerRevolution
-            self.listItemCardCornerRadius = listItemCardCornerRadius
-            self.listItemSquareImageCornerRadius = listItemSquareImageCornerRadius
-            self.questionFieldCardCornerRadius = questionFieldCardCornerRadius
-            self.questionFieldMinHeight = questionFieldMinHeight
-            self.questionFieldShadowRadius = questionFieldShadowRadius
-            self.questionFieldShadowYOffset = questionFieldShadowYOffset
         }
     }
 }
