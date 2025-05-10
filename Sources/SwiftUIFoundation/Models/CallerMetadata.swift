346f0576b26fcd48b7391cdf1ab570cf516c855c
Jason Lew-Rapai <jasonjrr@users.noreply.github.com>
2025-05-09 20:18:39 -0700

    Update CallerMetadata.swift

--- Sources/SwiftUIFoundation/Models/CallerMetadata.swift
+++ Sources/SwiftUIFoundation/Models/CallerMetadata.swift
@@ -5,12 +5,14 @@
 //  Created by Jason Lew-Rapai on 1/12/25.
 //
 
+import Foundation
 
 public struct CallerMetadata: Sendable {
     public let fileID: String
     public let function: String
     public let line: Int
     public let column: Int
+    public let timestamp: Date
     
     private let parentMetadataContainer: [CallerMetadata]?
     public var parentMetadata: CallerMetadata? {
@@ -23,12 +25,14 @@ public struct CallerMetadata: Sendable {
         function: String = #function,
         line: Int = #line,
         column: Int = #column,
+        timestamp: Date = Date(),
         parentMetadata: CallerMetadata? = nil
     ) {
         self.fileID = fileID
         self.function = function
         self.line = line
         self.column = column
+        self.timestamp = timestamp
         
         if let parentMetadata {
             self.parentMetadataContainer = [parentMetadata]
@@ -43,8 +47,9 @@ public struct CallerMetadata: Sendable {
         function: String = #function,
         line: Int = #line,
         column: Int = #column,
+        timestamp: Date = Date(),
         parentMetadata: CallerMetadata? = nil
     ) -> CallerMetadata {
-        CallerMetadata(fileID: fileID, function: function, line: line, column: column, parentMetadata: parentMetadata)
+        CallerMetadata(fileID: fileID, function: function, line: line, column: column, timestamp: timestamp, parentMetadata: parentMetadata)
     }
 }
